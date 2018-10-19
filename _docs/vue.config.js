1be34811b314482a2b4105bca214887440415910
mixingyu <crackermt@vip.qq.com>
2018-10-19 23:21:14 +0800

    docs: build

--- _docs/vue.config.js
+++ _docs/vue.config.js
@@ -66,7 +66,48 @@ module.exports = {
         postProcessHtml: function (context) {
           var titles = {// 在这里配置每个页面的标题
             '/': title,
-            '/Getting-Started': '快速开始' + ' ' + title
+            '/Getting-Started': '快速开始' + ' ' + title, 
+            
+            // 系统 System
+            '/Splashscreen': 'Splashscreen' + ' ' + title, 
+            '/StatusBar': 'StatusBar' + ' ' + title, 
+            '/Network': 'Network' + ' ' + title, 
+            '/Clipboard': 'Clipboard' + ' ' + title, 
+            '/JPush': 'JPush' + ' ' + title, 
+            '/Toast': 'Toast' + ' ' + title, 
+            '/Dialogs': 'Dialogs' + ' ' + title, 
+            '/File': 'File' + ' ' + title, 
+            '/FileTransfer': 'File Transfer' + ' ' + title, 
+            '/FileOpener2': 'FileOpener2' + ' ' + title, 
+            '/NativeAudio': 'Native Audio' + ' ' + title, 
+            '/ImagePicker': 'Image Picker' + ' ' + title, 
+            '/SMS': 'SMS' + ' ' + title, 
+            '/Contacts': 'Contacts' + ' ' + title, 
+            '/InAppBrowser': 'In App Browser' + ' ' + title, 
+            '/AppVersion': 'App Version' + ' ' + title, 
+            '/AppAvailability': 'App Availability' + ' ' + title, 
+            '/AppRate': 'App Rate' + ' ' + title, 
+            '/AppPreferences': 'App Preferences' + ' ' + title, 
+
+            // 设备 Device
+            '/Device': 'Device' + ' ' + title, 
+            '/BatteryStatus': 'Battery Status' + ' ' + title, 
+            '/Globalization': 'Globalization' + ' ' + title, 
+            '/Geolocation': 'Geolocation' + ' ' + title, 
+            '/Camera': 'Camera' + ' ' + title, 
+            '/Capture': 'Capture' + ' ' + title, 
+            '/Media': 'Media' + ' ' + title, 
+            '/BarcodeScanner': 'Barcode Scanner' + ' ' + title, 
+            '/DeviceOrientation': 'Device Orientation' + ' ' + title, 
+            '/DeviceMotion': 'Device Motion' + ' ' + title, 
+            '/Flashlight': 'Flashlight' + ' ' + title, 
+            '/Vibration': 'Vibration' + ' ' + title, 
+            '/iBeacon': 'iBeacon' + ' ' + title, 
+            '/Bluetooth': 'Bluetooth' + ' ' + title, 
+
+            // 其它 Other
+            '/AppLogin': 'App Login' + ' ' + title, 
+            '/SQLite': 'SQLite' + ' ' + title
           }
           return context.html.replace(
             /<title>[^<]*<\/title>/i,
